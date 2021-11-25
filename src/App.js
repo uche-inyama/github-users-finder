79e99ab008d2347694af26a6e46dd97457b2b0fe
uche-inyama <depoulet@yahoo.com>
2021-11-25 20:32:43 +0100

    Add NotFound page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/users';
-// import axios from 'axios';
 import './App.css';
 import GithubState from './components/context/github/GithubState';
 import AlertState from './components/context/alert/alertState';
-import Search from './components/users/search';
 import Alert from './components/layout/Alert';
 import About from './components/Pages/About';
 import User from './components/users/user';
 import Home from './components/Pages/home';
+import NotFound from './components/Pages/NotFound';
 
 const App = () => {
-
   return (
     <GithubState>
       <AlertState>
@@ -25,8 +22,8 @@ const App = () => {
                 <Routes>
                   <Route exact path='/' element={<Home />} />
                   <Route exact path="/about" element={<About />} />
-                  <Route path="/user/:login" element={<User />
-                 }/>
+                  <Route path="/user/:login" element={<User />}/>
+                  <Route element={<NotFound />} />
                 </Routes>
             </div>
           </div>
