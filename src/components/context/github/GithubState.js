ca4930a49bf79da976a56ee53961c8319326104c
Uchechukwu Henry Inyama <depoulet@yahoo.com>
2022-09-01 21:23:32 +0000

    Update GithubState.js

--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -30,21 +30,21 @@ const GithubState = props =>
     });
   };
 
-	const getUser = async (username) => {
-    	setLoading();
-    	const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${
-        process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
-          process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-      );
-	dispatch({
-	    type: GET_USER,
-	    payload: res.data
-	});
+ const getUser = async (username) => {
+    setLoading();
+    const res = await axios.get(
+   `https://api.github.com/users/${username}?client_id=${
+      process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+      process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    dispatch({
+       type: GET_USER,
+       payload: res.data
+     });
   };
-
-	const getUserRepos = async (username) => {
-        setLoading();
+ 
+  const getUserRepos = async (username) => {
+    setLoading();
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
@@ -52,17 +52,17 @@ const GithubState = props =>
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-	dispatch({
-	  type: GET_REPOS,
-	  payload: res.data
-	})
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    })
   };
 
 const getAlert = (msg, type) => {
-	dispatch({
-		type: SET_ALERT,
-		payload: { msg, type }
-	 });
+   dispatch({
+      type: SET_ALERT,
+      payload: { msg, type }
+   });
     // setAlert({msg, type});
     // setTimeout(() => setAlert(null), 5000);
   };
