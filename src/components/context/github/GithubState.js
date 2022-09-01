f94d8b9770772b7c9576968bccd0c9a3eea61311
Uchechukwu Henry Inyama <depoulet@yahoo.com>
2022-09-01 21:18:12 +0000

    Update GithubState.js

--- src/components/context/github/GithubState.js
+++ src/components/context/github/GithubState.js
@@ -4,14 +4,14 @@ import GithubContext from './githubContext';
 import GithubReducer from './githubReducer';
 import { SEARCH_USERS, SET_LOADING, SET_ALERT, CLEAR_USERS, GET_REPOS, GET_USER } from '../types';
 
-const GithubState = props => {
-	const initialState = {
-		users: [],
-    	user: {},
-		repos: [],
-		alert: {},
-		loading: false
-	}
+const GithubState = props => 
+   const initialState = {
+	users: [],
+	user: {},
+	repos: [],
+	alert: {},
+	loading: false
+    }
 
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
@@ -37,14 +37,14 @@ const GithubState = props => {
         process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
           process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
       );
-			dispatch({
-				type: GET_USER,
-				payload: res.data
-			});
+	dispatch({
+	    type: GET_USER,
+	    payload: res.data
+	});
   };
 
 	const getUserRepos = async (username) => {
-    setLoading();
+        setLoading();
 
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${
@@ -52,10 +52,10 @@ const GithubState = props => {
       ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-		dispatch({
-			type: GET_REPOS,
-			payload: res.data
-		})
+dispatch({
+  type: GET_REPOS,
+  payload: res.data
+})
   };
 
 	const getAlert = (msg, type) => {
